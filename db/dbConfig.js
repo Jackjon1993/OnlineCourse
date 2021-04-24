2880441ff771858f8d6507c9b96a725de82571a1
jackjon <lovejackjon@163.com>
2021-04-24 17:38:56 +0800

    update host

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -2,7 +2,7 @@ module.exports = {
     mysql: {
         host: 'localhost',
         user: 'root',
-        password: 'jackjon2020',
+        password: 'jackjon2021',
         database: 'myblog',
         port: '3306'
     }
